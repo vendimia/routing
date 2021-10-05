4f764b468d440094e940e9bbc1ce5c8b151573e2
Oliver Etchebarne <yo@drmad.org>
2021-10-05 16:29:45 -0500

    Add IS_REPEATABLE and TARGET_METHOD to MethodRoute classes

--- src/MethodRoute/Get.php
+++ src/MethodRoute/Get.php
@@ -3,7 +3,7 @@ namespace Vendimia\Routing\MethodRoute;
 
 use Attribute;
 
-#[Attribute]
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class Get extends MethodRouteAbstract
 {
     public function getHttpMethod()
